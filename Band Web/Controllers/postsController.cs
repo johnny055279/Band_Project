77189d106b62f5888224a92437403a8b854263c4
johnny055279 <39425477+johnny055279@users.noreply.github.com>
2020-10-08 16:12:03 +0800

    Set up Posts pages

---
+++ Band Web/Controllers/postsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Band_Web.Controllers
+{
+    public class postsController : Controller
+    {
+        // GET: posts
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
