11ae098ea744d37a6ec0f817c255db5ddd48db4e
johnny055279 <39425477+johnny055279@users.noreply.github.com>
2020-10-08 11:20:02 +0800

    Create RedisConnection.cs

---
+++ ClassLibrary/RedisConnection.cs
@@ -0,0 +1,40 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace ClassLibrary
+{
+    public sealed class RedisConnection
+    {
+        private static string _settingOption;
+        public readonly ConnectionMultiplexer ConnectionMultiplexer;
+
+        public static RedisConnection RedisConnectionInstance
+        {
+            get
+            {
+                return lazyRedisConnection.Value;
+            }
+        }
+
+        private RedisConnection()
+        {
+            //Set ConnectionMultiplexer
+            ConnectionMultiplexer = ConnectionMultiplexer.Connect(_settingOption);
+        }
+
+        public static void Init(string settingOption)
+        {
+            _settingOption = settingOption;
+        }
+
+        //Open RedisConnection until need it by using Lazy<> to cost down source
+        private static Lazy<RedisConnection> lazyRedisConnection = new Lazy<RedisConnection>(() =>
+        {
+            if (String.IsNullOrEmpty(_settingOption))
+            {
+                throw new InvalidOperationException("Please call Init() first.");
+            }
+            return new RedisConnection();
+        });
+    }
+}
