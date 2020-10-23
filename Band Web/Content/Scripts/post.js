c5c37b70c66ce949234c8d94003faab922a07b8b
johnny055279 <39425477+johnny055279@users.noreply.github.com>
2020-10-23 17:34:39 +0800

    update ckeditor5

--- Band Web/Content/Scripts/post.js
+++ Band Web/Content/Scripts/post.js
@@ -1,7 +1,86 @@
-﻿window.onload = function () {
-    ClassicEditor.create(document.querySelector('#editor')).catch(error => {
-        console.error(error);
-    });
+﻿class UploadAdapter {
+    constructor(loader) {
+        this.loader = loader;
+    }
+
+    upload() {
+        return new Promise((resolve, reject) => {
+            const reader = this.reader = new window.FileReader();
+            reader.addEventListener('load', () => { resolve({ default: reader.result }); })
+            reader.addEventListener('error', error => { reject(error); });
+            reader.addEventListener('abort', () => { reject(); });
+            this.loader.file.then(file => { reader.readAsDataURL(file); });
+        });
+    }
+
+    abort() {
+        this.reader.abort();
+    }
 }
 
+function AdapterPlugin(editor) {
+    editor.plugins.get('FileRepository').createUploadAdapter = (loader) => { return new UploadAdapter(loader) };
+}
 
+ClassicEditor.create(
+    document.querySelector('#editor'),
+    {
+        extraPlugins: [AdapterPlugin],
+		toolbar: {
+			items: [
+				'heading',
+				'|',
+				'bold',
+				'italic',
+				'link',
+				'bulletedList',
+				'numberedList',
+				'|',
+				'indent',
+				'outdent',
+				'|',
+				'imageUpload',
+				'imageInsert',
+				'mediaEmbed',
+				'|',
+				'insertTable',
+				'blockQuote',
+				'undo',
+				'redo',
+				'|',
+				'fontColor',
+				'fontSize',
+				'fontFamily',
+				'|',
+				'highlight',
+				'CKFinder'
+			]
+		},
+		language: 'en',
+		image: {
+			toolbar: [
+				'imageTextAlternative',
+				'imageStyle:full',
+				'imageStyle:side'
+			]
+		},
+		table: {
+			contentToolbar: [
+				'tableColumn',
+				'tableRow',
+				'mergeTableCells',
+				'tableCellProperties'
+			]
+		},
+		licenseKey: '',
+
+	})
+	.then(editor => {
+		window.editor = editor;
+    }
+).catch(error => {
+	console.error('Oops, something went wrong!');
+	console.error('Please, report the following error on https://github.com/ckeditor/ckeditor5/issues with the build id and the error stack trace:');
+	console.warn('Build id: 3gr4xvjhkhnk-scbtj9keuojb');
+	console.error(error);
+});
